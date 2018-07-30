306665e8a95ef385c6e0e79242a4b7ce62f01f12
SaxJ <saxon.jensen@gmail.com>
2018-07-30 10:37:59 +0800

    Filling out the cubie model.

--- src/Cubie/Edges.hs
+++ src/Cubie/Edges.hs
@@ -1,3 +1,13 @@
 module Cubie.Edges where
 
+import Cubie.Misc
+
+numEdges = 12
+
 data Orientation = Solved | Unsolved deriving (Bounded, Enum, Eq, Ord, Read, Show)
+
+newtype OrientationVector = OrientationVector { fromEdgeOrientation :: Vector Orientation }
+
+newtype PermutationVector = PermutationVector { fromEdgePermutation :: Vector Orientation }
+
+data Edge = Edge { perm :: PermutationVector, orien :: OrientationVector }
