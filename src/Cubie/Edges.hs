eb706532feec04253c0c2792cef3f14eee660e43
SaxJ <saxon.jensen@gmail.com>
2018-08-14 11:47:06 +0800

    Adding more to the cubie model.

--- src/Cubie/Edges.hs
+++ src/Cubie/Edges.hs
@@ -1,13 +1,22 @@
 module Cubie.Edges where
 
+import Prelude hiding (length)
 import Cubie.Misc
+import Data.Maybe
 
+numEdges :: Int
 numEdges = 12
 
-data Orientation = Solved | Unsolved deriving (Bounded, Enum, Eq, Ord, Read, Show)
+type Orientation = Int
 
 newtype OrientationVector = OrientationVector { fromEdgeOrientation :: Vector Orientation }
 
 newtype PermutationVector = PermutationVector { fromEdgePermutation :: Vector Orientation }
 
 data Edge = Edge { perm :: PermutationVector, orien :: OrientationVector }
+
+makePermutation :: Vector Orientation -> Maybe PermutationVector
+makePermutation v = if (length v) /= numEdges then Nothing else Just $ PermutationVector v
+
+makeOrientation :: Vector Orientation -> Maybe PermutationVector
+makeOrientation = makePermutation
