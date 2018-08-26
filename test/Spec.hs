ca718c925d4b60add6a86150020ff4b0e8144c17
jradtilbrook <jrad.tilbrook@gmail.com>
2018-08-26 21:08:40 +0800

    Configure Hspec to run tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,6 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+{-
+ - The line above instructs GHC to invoke hspec-discover as a pre-processor.
+ - The pre-processor scans for Spec files to run tests from.
+ -}
