70792f3382b699a57371acaac5bccbc9bb0511e2
jradtilbrook <jrad.tilbrook@gmail.com>
2018-08-26 21:17:51 +0800

    Add binaryFold function with tests

---
+++ src/Cubie/Heuristics/StateOne.hs
@@ -0,0 +1,9 @@
+module Cubie.Heuristics.StateOne
+( binaryFold
+) where
+
+{-
+ - This is used as a reducer for the edge orientation vector.
+ - It treats the vector as a binary number and converts it to decimal using little-endian.
+ -}
+binaryFold carry index value = carry + value * 2 ^ index
