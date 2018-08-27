14fac5f0e6ef1557f2edb1a9a74d1782049301a0
jradtilbrook <jrad.tilbrook@gmail.com>
2018-08-27 21:39:17 +0800

    Add common functions to Cubie.Moves module
    This includes a common accumulator for modifying orientation vectors,
    a permute function for modifying permutation vectors and a composition
    function to make applying sequential moves simpler.

---
+++ src/Cubie/Moves.hs
@@ -0,0 +1,26 @@
+module Cubie.Moves
+( accumulate
+, permute
+, (-:)
+) where
+
+import Data.Vector.Unboxed (accum, backpermute, fromList, Vector)
+
+{-
+ - Accumulate a vector using the provided array and a generic addition lambda with specific bound.
+ - This is used to modify elements of a vector based on the index and value pair.
+ -}
+accumulate modulus v s = accum (\a b -> (a + b) `mod` modulus) v
+
+{-
+ - Permute the given vector with the given sequence
+ - This is used to reorder a vector based on the values in the provided list.
+ -}
+permute v s = backpermute v $ fromList s
+
+{-
+ - Similar to function composition for applying moves to a cube.
+ - Start with the cube state, then write each move you want to take in order.
+ - Eg. Cube -: down -: right -: front
+ -}
+x -: f = f x
