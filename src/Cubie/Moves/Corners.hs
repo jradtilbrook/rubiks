51acea99edf5bafc5ae8e479a031ad147116fcb6
jradtilbrook <jrad.tilbrook@gmail.com>
2018-08-28 21:45:27 +0800

    Add separate modules for permuting edges and corners

---
+++ src/Cubie/Moves/Corners.hs
@@ -0,0 +1,73 @@
+module Cubie.Moves.Corners
+( front
+, back
+, left
+, right
+, up
+, down
+) where
+
+import qualified Cubie.Moves as M (accumulate, permute)
+
+-- set the bound on the accumulator
+accumulate = M.accumulate (3 :: Int)
+
+{-
+ - Apply a front face rotation to the given orientation vectors
+ -}
+front (cornOrien, cornPerm) =
+    (ov, pv)
+    where
+        ov = accumulate (M.permute cornOrien move) $ zip [1, 3, 4, 6] [1, 1, -1, -1]
+        pv = M.permute cornPerm move
+        move = [0, 6, 2, 4, 1, 5, 3, 7]
+
+{-
+ - Apply a back face rotation to the given orientation vectors
+ -}
+back (cornOrien, cornPerm) =
+    (ov, pv)
+    where
+        ov = accumulate (M.permute cornOrien move) $ zip [0, 2, 5, 7] [1, 1, -1, -1]
+        pv = M.permute cornPerm move
+        move = [7, 1, 5, 3, 4, 0, 6, 2]
+
+{-
+ - Apply a left face rotation to the given orientation vectors
+ -}
+left (cornOrien, cornPerm) =
+    (ov, pv)
+    where
+        ov = M.permute cornOrien move
+        pv = M.permute cornPerm move
+        move = [5, 4, 2, 3, 0, 1, 6, 7]
+
+{-
+ - Apply a right face rotation to the given orientation vectors
+ -}
+right (cornOrien, cornPerm) =
+    (ov, pv)
+    where
+        ov = M.permute cornOrien move
+        pv = M.permute cornPerm move
+        move = [0, 1, 7, 6, 4, 5, 2, 3]
+
+{-
+ - Apply a up face rotation to the given orientation vectors
+ -}
+up (cornOrien, cornPerm) =
+    (ov, pv)
+    where
+        ov = accumulate (M.permute cornOrien move) $ zip [0, 3, 4, 7] [-1, -1, 1, 1]
+        pv = M.permute cornPerm move
+        move = [4, 1, 2, 7, 3, 5, 6, 0]
+
+{-
+ - Apply a down face rotation to the given orientation vectors
+ -}
+down (cornOrien, cornPerm) =
+    (ov, pv)
+    where
+        ov = accumulate (M.permute cornOrien move) $ zip [1, 2, 5, 6] [-1, -1, 1, 1]
+        pv = M.permute cornPerm move
+        move = [0, 5, 6, 3, 4, 2, 1, 7]
---
+++ src/Cubie/Moves/Edges.hs
@@ -0,0 +1,73 @@
+module Cubie.Moves.Edges
+( front
+, back
+, left
+, right
+, up
+, down
+) where
+
+import qualified Cubie.Moves as M (accumulate, permute)
+
+-- set the bound on the accumulator
+accumulate = M.accumulate (2 :: Int) -- TODO: how do i fix type declarations so this cast isnt needed?
+
+{-
+ - Apply a front face rotation to the given orientation vectors
+ -}
+front (edgeOrien, edgePerm) =
+    (ov, pv)
+    where
+        ov = M.permute edgeOrien move
+        pv = M.permute edgePerm move
+        move = [0, 1, 2, 3, 4, 9, 8, 7, 5, 6, 10, 11]
+
+{-
+ - Apply a back face rotation to the given orientation vectors
+ -}
+back (edgeOrien, edgePerm) =
+    (ov, pv)
+    where
+        ov = M.permute edgeOrien move
+        pv = M.permute edgePerm move
+        move = [0, 1, 3, 2, 11, 5, 6, 1,0, 8, 9, 4, 7]
+
+{-
+ - Apply a left face rotation to the given orientation vectors
+ -}
+left (edgeOrien, edgePerm) =
+    (ov, pv)
+    where
+        ov = M.permute edgeOrien move
+        pv = M.permute edgePerm move
+        move = [4, 5, 3, 2, 1, 0, 6, 7, 8, 9, 10, 11]
+
+{-
+ - Apply a right face rotation to the given orientation vectors
+ -}
+right (edgeOrien, edgePerm) =
+    (ov, pv)
+    where
+        ov = M.permute edgeOrien move
+        pv = M.permute edgePerm move
+        move = [0, 1, 7, 6, 4, 5, 2, 3, 8, 9, 10, 11]
+
+{-
+ - Apply a up face rotation to the given orientation vectors
+ -}
+up (edgeOrien, edgePerm) =
+    (ov, pv)
+    where
+        ov = accumulate (M.permute edgeOrien move) $ zip [0, 3, 8, 11] [1, 1..]
+        pv = M.permute edgePerm move
+        move = [8, 1, 2, 11, 4, 5, 6, 7, 3, 9, 10, 0]
+
+{-
+ - Apply a down face rotation to the given orientation vectors
+ -}
+down (edgeOrien, edgePerm) =
+    (ov, pv)
+    where
+        ov = accumulate (M.permute edgeOrien move) $ zip [1, 2, 9, 10] [1, 1..]
+        pv = M.permute edgePerm move
+        move = [0, 10, 9, 3, 4, 5, 6, 7, 8, 1, 2, 11]
