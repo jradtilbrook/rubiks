ded8d5338e8cc60295afd14af102cec9de45bf14
jradtilbrook <jrad.tilbrook@gmail.com>
2018-08-29 20:41:45 +0800

    Selectively import functions from vector package

--- test/Cubie/Heuristics/StateOneSpec.hs
+++ test/Cubie/Heuristics/StateOneSpec.hs
@@ -2,19 +2,19 @@ module Cubie.Heuristics.StateOneSpec (spec) where
 
 import Test.Hspec
 import Cubie.Heuristics.StateOne
-import Data.Vector.Unboxed as V
+import Data.Vector.Unboxed (fromList, ifoldl)
 
 spec :: Spec
 spec = describe "binaryFold" $ do
     it "[0] equals 0" $
-        V.ifoldl binaryFold 0 (V.fromList [0 :: Int]) `shouldBe` 0
+        ifoldl binaryFold 0 (fromList [0 :: Int]) `shouldBe` 0
     it "[1] equals 1" $
-        V.ifoldl binaryFold 0 (V.fromList [1 :: Int]) `shouldBe` 1
+        ifoldl binaryFold 0 (fromList [1 :: Int]) `shouldBe` 1
     it "[0, 1] equals 2" $
-        V.ifoldl binaryFold 0 (V.fromList [0, 1 :: Int]) `shouldBe` 2
+        ifoldl binaryFold 0 (fromList [0, 1 :: Int]) `shouldBe` 2
     it "[1, 1, 0, 1] equals 11" $
-        V.ifoldl binaryFold 0 (V.fromList [1, 1, 0, 1 :: Int]) `shouldBe` 11
+        ifoldl binaryFold 0 (fromList [1, 1, 0, 1 :: Int]) `shouldBe` 11
     it "[1, 0, 1, 1, 0, 1] equals 45" $
-        V.ifoldl binaryFold 0 (V.fromList [1, 0, 1, 1, 0, 1 :: Int]) `shouldBe` 45
+        ifoldl binaryFold 0 (fromList [1, 0, 1, 1, 0, 1 :: Int]) `shouldBe` 45
     it "[1, 1, 1, 1, 1, 1, 1] equals 127" $
-        V.ifoldl binaryFold 0 (V.fromList [1, 1, 1, 1, 1, 1, 1 :: Int]) `shouldBe` 127
+        ifoldl binaryFold 0 (fromList [1, 1, 1, 1, 1, 1, 1 :: Int]) `shouldBe` 127
