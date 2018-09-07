fd3ab828d270588c75a523a731f033edad0595a1
jradtilbrook <jrad.tilbrook@gmail.com>
2018-09-07 22:30:18 +0800

    Remove unused functions from Cubie.Moves

--- src/Cubie/Moves.hs
+++ src/Cubie/Moves.hs
@@ -1,23 +1,4 @@
-module Cubie.Moves
-( accumulate
-, permute
-, (-:)
-) where
-
-import Data.Vector.Unboxed (accum, backpermute, fromList, Vector)
-
-{-
- - Accumulate a vector using the provided array and a generic addition lambda with specific bound.
- - This is used to modify elements of a vector based on the index and value pair.
- -}
-accumulate :: Int -> (Vector Int -> [(Int, Int)]-> Vector Int)
-accumulate modulus = accum (\a b -> (a + b) `mod` modulus)
-
-{-
- - Permute the given vector with the given sequence
- - This is used to reorder a vector based on the values in the provided list.
- -}
-permute v s = backpermute v $ fromList s
+module Cubie.Moves where
 
 {-
  - Similar to function composition for applying moves to a cube.
