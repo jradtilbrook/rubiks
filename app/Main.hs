631f7191fe5036cdfa62c2c6a323ae7243de6570
jradtilbrook <jrad.tilbrook@gmail.com>
2018-10-23 10:28:17 +0800

    Revert "Benchmark inverse move implementation using criterion"
    This reverts commit e49e66b8c0ca8f1429f994a15a710245cda97a6b.

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,6 @@
 module Main where
 
-import Criterion.Main
 import Cube
-import Moves.Edges
-import qualified Data.Vector.Unboxed as V
 
 main :: IO ()
-main = defaultMain [
-    bgroup "inverse front" [
-        bench "composition" $ whnf front'' solvedEdge,
-        bench "single" $ whnf front' solvedEdge
-        ]
-    ]
-
--- this is a test function that uses simple composition to call the front function three time to achieve an inverse
-front'' = front . front . front
-
--- this is a single function that combines the permutation vector to apply it in a single call
-front' (Edge edgeOrien edgePerm) =
-    Edge ov pv
-    where
-        ov = V.backpermute edgeOrien move
-        pv = V.backpermute edgePerm move
-        move = V.fromList [0, 1, 2, 3, 4, 8, 9, 7, 6, 5, 10, 11]
+main = print solvedCube
